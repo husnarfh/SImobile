e75b3fa9c9a02d704b5d7692a706c30eaa858370
Husna Nurarifah <husna.nna@gmail.com>
2018-12-10 23:08:11 +0700

    edit login dikit

--- sirubik/src/pages/login/login.ts
+++ sirubik/src/pages/login/login.ts
@@ -11,10 +11,7 @@ import { NgForm } from '@angular/forms';
   templateUrl: 'login.html'
 })
 export class LoginPage {
-  
-  loading: any;
-  login = { email:'', password:'' };
-  data: any;
+  data:any = {}; 
 
     constructor(
       public app: App,
@@ -25,7 +22,11 @@ export class LoginPage {
       public alertCtrl: AlertController,
       public events: Events,
       public http: Http) {
-
+        this.menuCtrl.enable(true);
+        this.data.email = "";
+        this.data.password = "";
+        this.data.response = "";
+        this.http = http;
     }
 
 
@@ -37,26 +38,33 @@ loader(){
       loader.present();
     }
 
-onLogin(form: NgForm) {
+login(){
+      var link = 'http://localhost/api/login';
+      var newLogin = JSON.stringify({email: this.data.email, password: this.data.password});
+       console.log(newLogin);
+      this.http.post(link, newLogin).subscribe(data => {
+        var response = data.json();
+            // this.data.response = data["_body"]; //https://stackoverflow.com/questions/39574305/property-body-does-not-exist-on-type-response
+        if(response.status == "200"){
+          console.log(response.data);
+         //this.data.login(response.data, "user");
+          this.loader();
+        this.app.getRootNav().setRoot(TabsPage);
+          // this.navCtrl.setRoot(ProfilPage);
+        } else {
+          // If account not found
+          var toast = this.toastCtrl.create({
+            message: 'Incorrect username or password',
+            duration: 3000,
+            position: 'bottom'
+          });
+          toast.present();
+        }
+      }, error => {
+        console.log("Oooops!");
+      });
+  }
 
-      if (form.valid) {
-  
-        let loading = this.loadCtrl.create({});
-        loading.present();
-  
-        this.data.login(this.login).subscribe(data => {
-            this.events.publish('user:login');
-            loading.dismiss();
-            this.navCtrl.setRoot(TabsPage);
-          },
-          error => {
-            loading.dismiss();
-            this.showError(error);
-        });
-        
-      }
-    }
-  
 signIn(){
   this.navCtrl.push(TabsPage);
 }
